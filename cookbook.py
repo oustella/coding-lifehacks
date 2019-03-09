f4bd2053ff7affeed2c32b3de0795c3d69903d30
oustella <oustella@users.noreply.github.com>
2019-03-09 14:44:57 -0500

    Create cookbook.py

---
+++ cookbook.py
@@ -0,0 +1,7 @@
+# Print the entire numpy array
+np.set_printoptions(threshold=np.nan)
+
+### Jupyter Notebook related ###
+# Print multiple outputs in one cell
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
