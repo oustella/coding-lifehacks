2dde1bf4d3f5b85357e9896a8128e474a0f61628
oustella <oustella@users.noreply.github.com>
2019-04-21 15:13:58 -0400

    added metho for uploading a project to github

--- terminal.sh
+++ terminal.sh
@@ -5,6 +5,14 @@ git fetch upstream
 git checkout master
 git merge upstream/master
 
+# Upload an existing local project to a git repository
+# In the terminal of your local folder:
+git init  # initiate git for your local folder
+git remote add origin https://github.com/userNAME/repoName.git
+git commit -m "initial commit" # this is necessary for first time uploading
+git pull origin master --allow-unrelated-histories # this is optional if you already have files in the online repo
+git push origin master
+
 # Open Jupyter notebook in desired web browser
 jupyter lab --browser safari
 
